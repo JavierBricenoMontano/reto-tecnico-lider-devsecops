cb89fb66b596c5263dca321f756a08c231b0bd48
JavierBricenoMontano <92614527+JavierBricenoMontano@users.noreply.github.com>
2025-09-28 01:30:54 -0500

    Update app.js

--- mibanco-microservice/src/app.js
+++ mibanco-microservice/src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hola Mibanco :)' });
+  res.json({ message: 'Hola Mibanco :) 2025' });
 });
 
 app.get('/health', (req, res) => {
